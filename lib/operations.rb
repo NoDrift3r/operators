777600bd4c8645b4cfdcf342dedc4f6a40ff0224
Jesus <jose.jesus16@gmail.com>
2021-03-05 23:02:16 -0500

    commit tree a80ef647d7766837f620023aebfd6e3d561c4180

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,26 @@
+#require 'pry' 
 def unsafe?(speed)
+ 
+ if speed > 60
+    true
+ elsif speed < 40
+    true
+ else speed > 40 && speed < 60 
+     false
+ end 
 
 end
 
 
-
-def not_safe?(speed)
-	
+ def not_safe?(speed)
+	if speed > 60
+   # binding.pry
+    true
+  elsif speed < 40 
+    true
+  else
+  speed > 40 && speed < 60 ? false : true
+  end 
 end
 	
 
